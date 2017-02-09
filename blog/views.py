5ea6b8092d8f7b681f4fa7dfc34d14bcd342dd1e
mdolata <mateuszdolata2@gmail.com>
2017-02-09 23:00:14 +0100

    dodanie strony startowej

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+
+def home(request):
+    return render(request, 'blog/home.html', {})
